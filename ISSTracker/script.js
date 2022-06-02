68b49cc74f5290b833fbe7cc83150e828640d001
bilgramiraza <bilgramiraza@yahoo.com>
2022-06-02 16:58:59 +0530

    Implemented LeafletJS with ISS api to display location on a timer

--- ISSTracker/script.js
+++ ISSTracker/script.js
@@ -5,11 +5,30 @@ async function fetchISSLocation(){
   const {latitude, longitude} = data;
   return data;
 }
-const reloadBtn = document.querySelector('#reload');
-reloadBtn.addEventListener('click',async ()=>{
+function ISSmap() {
+  const myMap = L.map('ISSmap').setView([0,0], 1);
+  const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  const tiles = L.tileLayer(tileURL, {attribution});
+  tiles.addTo(myMap);
+  const issIcon = L.icon({
+      iconUrl: 'iss.png',
+      iconSize: [50, 32],
+      iconAnchor: [25, 16],
+  });
+  return [L.marker([0,0], {icon:issIcon}).addTo(myMap), myMap];
+}
+async function updateMarker(){
   const {latitude, longitude} = await fetchISSLocation();
   const latDOM = document.querySelector('#lat');
   const lonDOM = document.querySelector('#lon');
-  latDOM.textContent = latitude;
-  lonDOM.textContent = longitude;
-});
+  latDOM.textContent = latitude.toFixed(2);
+  lonDOM.textContent = longitude.toFixed(2);
+  marker.setLatLng([latitude, longitude]);
+  myMap.setView([latitude, longitude], 5);
+}
+const reloadBtn = document.querySelector('#reload');
+const [marker, myMap] = ISSmap();
+updateMarker();
+setInterval(updateMarker, 10000);
+reloadBtn.addEventListener('click',updateMarker);
