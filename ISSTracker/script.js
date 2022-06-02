7bb52d4e520eaa3344e793ce78d6ebc963dba66f
bilgramiraza <bilgramiraza@yahoo.com>
2022-06-02 13:46:46 +0530

    Fetches Location

--- ISSTracker/script.js
+++ ISSTracker/script.js
@@ -1,7 +1,15 @@
 async function fetchISSLocation(){
+  console.log('loading Data');
   const response = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
   const data = await response.json();
   const {latitude, longitude} = data;
-  console.log(latitude, longitude);
+  return data;
 }
-fetchISSLocation();
+const reloadBtn = document.querySelector('#reload');
+reloadBtn.addEventListener('click',async ()=>{
+  const {latitude, longitude} = await fetchISSLocation();
+  const latDOM = document.querySelector('#lat');
+  const lonDOM = document.querySelector('#lon');
+  latDOM.textContent = latitude;
+  lonDOM.textContent = longitude;
+});
