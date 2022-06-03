383b2420f186dc57196bae6d3987f6dc297c8c88
bilgramiraza <bilgramiraza@yahoo.com>
2022-06-03 21:44:48 +0530

    EOD commit

--- dataSelfie/index.js
+++ dataSelfie/index.js
@@ -2,4 +2,16 @@ const express = require('express');
 const app = express();
 app.listen(3000,()=>console.log('listening at port 3000'));
 app.use(express.static('public'));
+app.use(express.json({limit:'1mb'}));
+let coords = [];
+app.post('/api',(request, response)=>{
+  coords.push({timeStamp:request.body.time,lat:request.body.lat,lon:request.body.lon});
+  console.log(coords);
+  response.json({
+    status:'success',
+    latitude:request.body.lat,
+    longitude:request.body.lon,
+    timestamp:request.body.time,
+  });
+});
 
--- dataSelfie/public/script.js
+++ dataSelfie/public/script.js
@@ -1,13 +1,30 @@
-if('geolocation' in navigator) {
-  console.log('Geolocation Available');
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position);
-    const lat = document.querySelector('.lat');
-    const lon = document.querySelector('.lon');
-    lat.textContent = position.coords.latitude;
-    lon.textContent = position.coords.longitude;
-  });
-} else {
-  /* geolocation IS NOT available */
-  console.log('Geolocation Not Available');
+function recordCoords() {
+  if('geolocation' in navigator) {
+    console.log('Geolocation Available');
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+      const lat = document.querySelector('.lat');
+      const lon = document.querySelector('.lon');
+      lat.textContent = position.coords.latitude;
+      lon.textContent = position.coords.longitude;
+      sendData(position.timestamp,position.coords.latitude,position.coords.longitude);
+    });
+  } else {
+    /* geolocation IS NOT available */
+    console.log('Geolocation Not Available');
+  } 
 }
+async function sendData(time,lat,lon) {
+  const data = {time,lat,lon};
+  const options = {
+    method:'POST',
+    headers:{
+      "Content-Type": 'application/json',
+    },
+    body:JSON.stringify(data),
+  };
+  const response = await fetch('/api',options);
+  console.log(await response.json());
+}
+const recordBtn = document.querySelector('#record');
+recordBtn.addEventListener('click',recordCoords);
