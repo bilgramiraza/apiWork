0a11e556508dc9ff63ce87c51661862545b4698a
bilgramiraza <bilgramiraza@yahoo.com>
2022-06-04 11:44:05 +0530

    implemented basic Database

--- dataSelfie/index.js
+++ dataSelfie/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 app.listen(3000,()=>console.log('listening at port 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
-let coords = [];
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.post('/api',(request, response)=>{
-  coords.push({timeStamp:request.body.time,lat:request.body.lat,lon:request.body.lon});
-  console.log(coords);
+  let coords = {
+    timeStamp:Date.now(),
+    requestTimeStamp:request.body.time,
+    lat:request.body.lat,
+    lon:request.body.lon
+  };
+  database.insert(coords);
   response.json({
     status:'success',
     latitude:request.body.lat,
